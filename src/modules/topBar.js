41ebaef7a285b63fde6a0f7a6005467604b38a46
Misha Kuptsov <azazin1955@gmail.com>
2024-07-15 16:32:33 +0100

    new project

---
+++ src/modules/topBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './topBar.scss';
+
+function TopBar() {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-logo">
+        <h1>Мой сайт</h1>
+      </div>
+      <div className="top-bar-menu">
+        <a href="#menus">Our Menus</a>
+        <a href="#offers">Offers</a>
+        <a href="#steak-cocktail">Steak & Cocktail</a>
+        <a href="#book">Book a Table</a>
+      </div>
+    </div>
+  );
+}
+
+export default TopBar;
